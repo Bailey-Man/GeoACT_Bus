1749ee7a8fd7a56bebb51bd85d565b290f438e58
Bailey-Man <bmman@ucsd.edu>
2021-05-15 18:26:50 -0700

    asg

--- av.py
+++ av.py
@@ -139,16 +139,20 @@ class user_viz():
         '''
         bus_seating = self.generate_bus_seating()
         bus_trip, conc_array, out_mat, chance_nonzero = bus_sim(int(self.students_var), self.mask_var, self.number_simulations, self.trip_length, self.seat_var) # replace default with selected
-        # print('conc_heat_figure')
-        # plt.figure(figsize=(5,4))
-        # plt.subplots()
-        # base = plt.gca().transData
-        # fig = plt.subplots() # ????
+
+        seating = self.generate_bus_seating()
+        x_arr = []
+        y_arr = []
+        for i in seating.items():
+            x_arr.append(i[1][1])
+            y_arr.append(i[1][0])
+
         rot = mpl.transforms.Affine2D().rotate_deg(180)
         fig = plt.figure()
         ax1 = fig.add_subplot(1, 2, 1)
         ax1.matshow(out_mat, cmap="OrRd", norm=mpl.colors.LogNorm())
         plt.arrow(-2,24,0,-26, head_width=0.2, head_length=0.2, fc='k', ec='k')
+        plt.scatter(x=x_arr, y=y_arr)
         plt.gcf().set_size_inches(2,2)
         # plt.suptitle('Relative Airflow Heatmap', fontsize=7.5)
         plt.annotate(xy=(-1, -1), text='front', fontsize=5)
@@ -159,13 +163,14 @@ class user_viz():
         ax2 = fig.add_subplot(1,2,2)
         ax2.matshow(out_mat, cmap="OrRd")#, norm=mpl.colors.LogNorm())
         plt.arrow(-2,24,0,-26, head_width=0.2, head_length=0.2, fc='k', ec='k')
+        plt.scatter(x=x_arr, y=y_arr)
         plt.gcf().set_size_inches(2,2)
         plt.suptitle('Relative Airflow Heatmap', fontsize=7.5)
         plt.annotate(xy=(-1, -1), text='front', fontsize=5)
         plt.annotate(xy=(-1, 24), text='back', fontsize=5)
         # log scale vs regular scale + 'be not afraid'
         plt.axis('off')
-        fig.text(.1, .01, 'These heatmaps show relative airflow within the cabin \nof the bus in terms of its effect on concentration \nof COVID-19 Particles (averaged across 100 simulations)', fontsize=4)
+        fig.text(.1, .01, 'These heatmaps show relative airflow within the cabin \nof the bus in terms of its effect on concentration \nof COVID-19 Particles', fontsize=4)
         plt.savefig('results/relative_airflow.png', dpi=300)
 
         print('relative airflow complete!')
